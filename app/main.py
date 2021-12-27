6e05196d4cad47ed3048265e099cff7fdd376ab8
Michal Gurgul <m.gurgul@telecube.pl>
2021-12-27 12:45:08 +0100

    Tests: Initial setup

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-import asyncio
+# import asyncio
 import random
 from typing import Optional
 
---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+from starlette.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_ping():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
