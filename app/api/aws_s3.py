7eb594ed6765122ccd7b76c9869df64b073a0371
Michał Gurgul <mgurgul@telecube.pl>
2022-01-11 21:37:55 +0100

    AWS basic support

--- app/api/aws_s3.py
+++ app/api/aws_s3.py
@@ -32,42 +32,46 @@ s3_client = boto3.client(
 async def post_create_bucket():
 
     prefix = "mgu"
-    bucket_name = prefix + "-" + str(uuid.uuid4().hex)
+    bucket_name = prefix + "-" + "dc5b9aefbee54953824d9fc327df7faf"  # str(uuid.uuid4().hex)
+    # mgu-dc5b9aefbee54953824d9fc327df7faf
     location = {"LocationConstraint": settings.s3_region}
 
-    # print("######")
-    # print(bucket_name)
+    try:
+        response = s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=location)
+    except BaseException as error:
+        print(error)
 
-    response = s3_client.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=location)
-
-    # print(response)
     return bucket_name
 
 
 @s3_router.get("/list_buckets")
 async def get_buckets_list():
-
+    s3_buckets = []
     response = s3_client.list_buckets()
 
     print("Listing Amazon S3 Buckets:")
 
     for bucket in response["Buckets"]:
+        s3_buckets.append(bucket["Name"])
         print(f"-- {bucket['Name']}")
-
-
-# -------------------------------------------
-
-
-@s3_router.get("/get_bucket_names")
-async def get_all_bucket_names():
-    s3_buckets = []
-    for bucket in s3_resource.buckets.all():
-        s3_buckets.append(bucket.name)
     return s3_buckets
 
 
-@s3_router.post("/get_s3_obj/")
-async def get_s3(s3_obj: str):
+@s3_router.delete("/bucket/{bucket_name}")
+async def remove_bucket(bucket_name: str):
+
+    for s3_object in s3_resource.Bucket(bucket_name).objects.all():
+        s3_object.delete()
+    # Deleting objects versions if S3 versioning enabled
+    for s3_object_ver in s3_resource.Bucket(bucket_name).object_versions.all():
+        s3_object_ver.delete()
+
+    response = s3_client.delete_bucket(Bucket=bucket_name)
+    return response
+
+
+@s3_router.get("/get_s3_obj/")
+async def get_s3(s3_obj: str, bucket_name: str = "manuto"):
     """
     Retreives an s3 jpg image and streams it back.
     ### Request Body
@@ -81,71 +85,49 @@ async def get_s3(s3_obj: str):
     s3_resource.Bucket(settings.s3_bucket_name).download_fileobj(s3_obj, f)
     f.seek(0)
     return StreamingResponse(
-        f, media_type="image/jpg", headers={"Content-Disposition": 'inline; filename="%s.jpg"' % (object,)}
+        f, media_type="image/jpg", headers={"Content-Disposition": f'inline; filename="{s3_obj}"'}
     )
 
 
-@s3_router.post("/upload/")
-async def upload_aws_s3(file: UploadFile = File(...)):
+@s3_router.post("/upload/{objectName}")
+async def upload_aws_s3(objectName: str, file: UploadFile = File(...)):
 
     # https://www.youtube.com/watch?v=JKlOlDFwsao
     # https://github.com/search?q=upload_fileobj+fastapi&type=code
 
-    # bucket names
-    for bucket in s3_resource.buckets.all():
-        print(bucket.name)
-
     s3_resource.Bucket(settings.s3_bucket_name).upload_fileobj(
         Fileobj=file.file,
-        Key="img.png",
+        Key=objectName,
         # ExtraArgs={
         #     "ContentType": "image/png",
         #     "ACL": "public-read",
         # },
     )
 
-    # session = Session(aws_access_key_id=settings.s3_access_key, aws_secret_access_key=settings.s3_secret_access_key)
-    # s3 = session.resource("s3")
-    # your_bucket = s3.Bucket(settings.s3_bucket_name)
-
-    # filename = file.filename
-    # data = file.file._file
-
-    # s3.Bucket(settings.s3_bucket_name).upload_file(key=filename, fileobject=data)
-
-    # for s3_file in your_bucket.objects.all():
-    #     print(s3_file.key)
-
-    # s3 = S3Client(
-    #     AsyncClient,
-    #     S3Config(
-    #         settings.s3_access_key, settings.s3_secret_access_key, settings.s3_region, settings.s3_bucket_name + ".com"
-    #     ),
-    # )
-    # await s3.upload("path/to/upload-to.txt", b"this the content")
-
-    # files = [f for f in s3.list()]
-
-    # # print(settings.s3_region)
-    # print(settings.s3_access_key, settings.s3_secret_access_key, settings.s3_region, settings.s3_bucket_name)
     return {"region": settings.s3_region, "files": "files", "filename": file.filename}
-    # return {"filename": file.filename}
 
 
-@s3_router.get("/sign")
+@s3_router.get("/upload_signed_url")
 def sign_s3_upload(objectName: str):
 
-    file_name = "file_name"
-    file_type = "text/plain"
+    try:
+        url = s3_client.generate_presigned_url(
+            "put_object",
+            Params={"Bucket": settings.s3_bucket_name, "Key": objectName},
+            ExpiresIn=3600,
+            HttpMethod="PUT",
+        )
+    except BaseException as error:
+        return None
 
-    s3 = boto3.client("s3")
+    return url
 
-    presigned_post = s3.generate_presigned_post(
-        Bucket=settings.s3_bucket_name,
-        Key=file_name,
-        Fields={"acl": "public-read", "Content-Type": file_type},
-        Conditions=[{"acl": "public-read"}, {"Content-Type": file_type}],
-        ExpiresIn=3600,
+
+@s3_router.get("/download_signed_url")
+def sign_s3_download(objectName: str):
+
+    url = s3_client.generate_presigned_url(
+        ClientMethod="get_object", Params={"Bucket": settings.s3_bucket_name, "Key": "TPNE.jpeg"}, ExpiresIn=3600
     )
 
-    return {"data": presigned_post, "url": "https://%s.s3.amazonaws.com/%s" % (settings.s3_bucket_name, file_name)}
+    return url
