79f7a72619d4f40b580b10090a8488af9860b38c
Michal Gurgul <m.gurgul@telecube.pl>
2022-02-18 16:53:03 +0100

    Event index

--- app/api/events.py
+++ app/api/events.py
@@ -18,23 +18,135 @@ from app.models.models import Events, Tasks
 event_router = APIRouter()
 
 
-@event_router.get("/index", name="user:Profile")
-async def file_get_all(*, session: Session = Depends(get_session)):
-    uuid = "fd36ef87-f717-457b-a667-75d314edcd1d"
-    event = session.exec(select(Events).where(Events.uuid == uuid)).one_or_none()
+@event_router.get("/index", name="event:Profile")
+async def file_get_all(
+    *,
+    session: Session = Depends(get_session),
+    dt_from="2022-02-01",
+    dt_to="2022-02-28",
+):
+    task = session.exec(select(Tasks).where(Tasks.event_id.in_([1, 2]))).all()
+    tsk_dict = {}
+    tsk = list(task)
+    for t in tsk:
+        tsk_dict[t.event_id] = t.title
+        print(t.event_id)
+    print(tsk_dict)
 
-    task = session.exec(select(Tasks).where(Tasks.event_id == 6)).one_or_none()
-    print(task.title)
+    events_dict = []
+    events = session.exec(select(Events).where(Events.start_at >= dt_from)).all()
+    for event in events:
+        ev = event.dict()
 
-    events = []
-    details = {}
-    details["title"] = task.title
+        if ev["unit"] == "YEARLY":
+            freq = 0
+        if ev["unit"] == "MONTHLY":
+            freq = 1
+        if ev["unit"] == "WEEKLY":
+            freq = 2
+        if ev["unit"] == "DAILY":
+            freq = 3
 
-    events.append(details)
-    events.append({"title": "ONE", "desc": "Oh"})
-    events.append({"title": "TWO", "desc": "Oh"})
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(json.dumps(events))
+        interval = ev["interval"]
+
+        days = []
+        if ev["at_mo"] == True:
+            days.append(0)
+        if ev["at_tu"] == True:
+            days.append(1)
+        if ev["at_we"] == True:
+            days.append(2)
+        if ev["at_th"] == True:
+            days.append(3)
+        if ev["at_fr"] == True:
+            days.append(4)
+        if ev["at_sa"] == True:
+            days.append(5)
+        if ev["at_su"] == True:
+            days.append(6)
+
+        dt_start = ev["start_at"]
+
+        rule = rrule(freq=freq, interval=interval, byweekday=days, count=31, dtstart=dt_start)
+        dt_after = pendulum.from_format(dt_from, "YYYY-MM-DD", tz="UTC")
+        dt_before = pendulum.from_format(dt_to, "YYYY-MM-DD", tz="UTC")
+        gen_events = rule.between(after=dt_after, before=dt_before, inc=True)
+
+        for e in gen_events:
+            details = {}
+            details["title"] = tsk_dict[ev["id"]]
+            # details["uuid"] = str(task.uuid)
+            details["event_date"] = pendulum.instance(e, tz="UTC").to_iso8601_string()
+            events_dict.append(details)
+
+    # https://dateutil.readthedocs.io/en/stable/rrule.html
+
+    # daily = rrule(freq=DAILY, interval=2, count=31, dtstart=dt_start)
+    # workdays = rrule(freq=WEEKLY, byweekday=[0, 1, 2, 3, 4], count=31, dtstart=dt_start)
+    # weekend = rrule(freq=WEEKLY, byweekday=[5, 6], count=31, dtstart=dt_start)
+    # weekly = rrule(freq=WEEKLY, interval=2, byweekday=[5], count=31, dtstart=dt_start)
+    # weekly_at_days = rrule(freq=WEEKLY, byweekday=[0, 2, 4], count=31, dtstart=dt_start)
+    # monthly = rrule(freq=MONTHLY, interval=2, count=31, dtstart=dt_start)
+    # yearly = rrule(freq=YEARLY, interval=2, count=31, dtstart=dt_start)
+
+    return events_dict
+
+
+@event_router.get("/{uuid}", name="event:Profile")
+async def file_get_all(
+    *,
+    session: Session = Depends(get_session),
+    dt_from="2022-02-01",
+    dt_to="2022-02-28",
+    uuid="bd426648-fd93-49bb-a363-904be5bf89f0",
+):
+    uuid = "bd426648-fd93-49bb-a363-904be5bf89f0"
+    task = session.exec(select(Tasks).where(Tasks.uuid == uuid)).one_or_none()
+    event = session.exec(select(Events).where(Events.id == task.event_id)).one_or_none()
+    ev = event.dict()
+
+    if ev["unit"] == "YEARLY":
+        freq = 0
+    if ev["unit"] == "MONTHLY":
+        freq = 1
+    if ev["unit"] == "WEEKLY":
+        freq = 2
+    if ev["unit"] == "DAILY":
+        freq = 3
+
+    interval = ev["interval"]
+
+    days = []
+    if ev["at_mo"] == True:
+        days.append(0)
+    if ev["at_tu"] == True:
+        days.append(1)
+    if ev["at_we"] == True:
+        days.append(2)
+    if ev["at_th"] == True:
+        days.append(3)
+    if ev["at_fr"] == True:
+        days.append(4)
+    if ev["at_sa"] == True:
+        days.append(5)
+    if ev["at_su"] == True:
+        days.append(6)
+
+    dt_start = ev["start_at"]
+
+    rule = rrule(freq=freq, interval=interval, byweekday=days, count=31, dtstart=dt_start)
+    dt_after = pendulum.from_format(dt_from, "YYYY-MM-DD", tz="UTC")
+    dt_before = pendulum.from_format(dt_to, "YYYY-MM-DD", tz="UTC")
+    gen_events = rule.between(after=dt_after, before=dt_before, inc=True)
+
+    events_dict = []
+
+    for e in gen_events:
+        details = {}
+        details["title"] = task.title
+        details["uuid"] = str(task.uuid)
+        details["event_date"] = pendulum.instance(e, tz="UTC").to_iso8601_string()
+        events_dict.append(details)
 
     #   events: [
     # {
@@ -52,54 +164,10 @@ async def file_get_all(*, session: Session = Depends(get_session)):
     # },
 
     # ev = dict(event)
-
-    ev = event.dict()
-    pp.pprint(ev)
-
-    events = {}
-    events["0"] = {}
-    events["0"]["id"] = ev["id"]
-
-    dt_start = ev["start_at"]
-    dt_after = pendulum.from_format("2022-02-16 22", "YYYY-MM-DD HH", tz="Europe/Warsaw")
-    dt_before = pendulum.from_format("2022-12-20 22", "YYYY-MM-DD HH", tz="Europe/Warsaw")
-
-    if ev["unit"] == "YEARLY":
-        freq = 0
-    if ev["unit"] == "MONTHLY":
-        freq = 1
-    if ev["unit"] == "WEEKLY":
-        freq = 2
-    if ev["unit"] == "DAILY":
-        freq = 3
-
-    interval = ev["interval"]
-    days = []
-    if ev["at_mo"] == True:
-        days.append(0)
-    if ev["at_tu"] == True:
-        days.append(1)
-    if ev["at_we"] == True:
-        days.append(2)
-    if ev["at_th"] == True:
-        days.append(3)
-    if ev["at_fr"] == True:
-        days.append(4)
-    if ev["at_sa"] == True:
-        days.append(5)
-    if ev["at_su"] == True:
-        days.append(6)
-
-    rule = rrule(freq=freq, interval=interval, byweekday=days, count=31, dtstart=dt_start)
-    times = rule.between(after=dt_after, before=dt_before, inc=True)
-    print("##################")
-    for t in times:
-        print(t)
-    print("##################")
-    pp.pprint(events)
-
-    # for ev in event:
-    #     pp.pprint(ev.dict())
+    # print("##################")
+    # pp = pprint.PrettyPrinter(indent=4)
+    # pp.pprint(events_dict)
+    # print("##################")
 
     # https://dateutil.readthedocs.io/en/stable/rrule.html
 
@@ -115,24 +183,16 @@ async def file_get_all(*, session: Session = Depends(get_session)):
     # Co miesiąc - rrule(freq=MONTHLY, count=31, dtstart=dt_start)
     # Co roku  - rrule(freq=YEARLY, count=31, dtstart=dt_start)
 
-    dt_start = pendulum.from_format("2022-02-15 22", "YYYY-MM-DD HH", tz="Europe/Warsaw")
-    dt_after = pendulum.from_format("2022-02-16 22", "YYYY-MM-DD HH", tz="Europe/Warsaw")
-    dt_before = pendulum.from_format("2022-12-20 22", "YYYY-MM-DD HH", tz="Europe/Warsaw")
+    # dt_start = pendulum.from_format("2022-02-15 22", "YYYY-MM-DD HH", tz="Europe/Warsaw")
+    # dt_after = pendulum.from_format("2022-02-16 22", "YYYY-MM-DD HH", tz="Europe/Warsaw")
+    # dt_before = pendulum.from_format("2022-12-20 22", "YYYY-MM-DD HH", tz="Europe/Warsaw")
 
-    daily = rrule(freq=DAILY, interval=2, count=31, dtstart=dt_start)
-    workdays = rrule(freq=WEEKLY, byweekday=[0, 1, 2, 3, 4], count=31, dtstart=dt_start)
-    weekend = rrule(freq=WEEKLY, byweekday=[5, 6], count=31, dtstart=dt_start)
-    weekly = rrule(freq=WEEKLY, interval=2, byweekday=[5], count=31, dtstart=dt_start)
-    weekly_at_days = rrule(freq=WEEKLY, byweekday=[0, 2, 4], count=31, dtstart=dt_start)
-    monthly = rrule(freq=MONTHLY, interval=2, count=31, dtstart=dt_start)
-    yearly = rrule(freq=YEARLY, interval=2, count=31, dtstart=dt_start)
+    # daily = rrule(freq=DAILY, interval=2, count=31, dtstart=dt_start)
+    # workdays = rrule(freq=WEEKLY, byweekday=[0, 1, 2, 3, 4], count=31, dtstart=dt_start)
+    # weekend = rrule(freq=WEEKLY, byweekday=[5, 6], count=31, dtstart=dt_start)
+    # weekly = rrule(freq=WEEKLY, interval=2, byweekday=[5], count=31, dtstart=dt_start)
+    # weekly_at_days = rrule(freq=WEEKLY, byweekday=[0, 2, 4], count=31, dtstart=dt_start)
+    # monthly = rrule(freq=MONTHLY, interval=2, count=31, dtstart=dt_start)
+    # yearly = rrule(freq=YEARLY, interval=2, count=31, dtstart=dt_start)
 
-    rule = daily
-
-    times = rule.between(after=dt_after, before=dt_before, inc=True)
-    print("##################")
-    # for t in times:
-    #     print(t)
-    print("##################")
-
-    return "OK"
+    return events_dict
