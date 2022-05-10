e657566885b33e3c97575cc83f7a0b558193bf70
mgur <mgur@pop-os.localdomain>
2022-05-10 17:44:59 +0200

    settings

--- app/api/settings.py
+++ app/api/settings.py
@@ -11,18 +11,26 @@ from app.service.bearer_auth import has_token
 setting_router = APIRouter()
 
 
-@setting_router.get("/", response_model=List[SettingAddIn], name="settings:List")
-async def setting_get_all(*, session: Session = Depends(get_session), auth=Depends(has_token)):
+@setting_router.get("/", name="settings:List")  # response_model=List[SettingAddIn],
+async def setting_get_all(
+    *, session: Session = Depends(get_session), setting_names: List[str] = Query(None), auth=Depends(has_token)
+):
 
     db_setting = session.exec(select(Settings).where(Settings.account_id == auth["account"])).all()
 
     if not db_setting:
         raise HTTPException(status_code=404, detail="Settings not found")
 
-    return db_setting
+    res = {}
+    for elt in db_setting:
+        # res.setdefault(elt.entity, []).append(elt.value) //
+        res[elt.entity] = elt.value
+    # print(res)
+
+    return res
 
 
-@setting_router.get("/{setting_name}", response_model=SettingAddIn, name="settings:List")
+@setting_router.get("/{setting_name}", name="settings:List")  # response_model=SettingAddIn,
 async def setting_get_all(*, session: Session = Depends(get_session), setting_name: str, auth=Depends(has_token)):
 
     db_setting = session.exec(
@@ -32,15 +40,24 @@ async def setting_get_all(*, session: Session = Depends(get_session), setting_na
     if not db_setting:
         raise HTTPException(status_code=404, detail="Setting not found")
 
-    return db_setting
+    res = {}
+    # for elt in db_setting:
+    # res.setdefault(elt.entity, []).append(elt.value) //
+    res[db_setting.entity] = db_setting.value
+    # print(res)
+
+    return res
 
 
 @setting_router.post("/", response_model=StandardResponse, name="settings:Add")
 async def setting_add(*, session: Session = Depends(get_session), setting: SettingAddIn, auth=Depends(has_token)):
 
-    allowed_settings = ["mode"]
+    allowed_settings = ["idea_registration_mode", "issue_registration_email"]
 
     res = SettingAddIn.from_orm(setting)
+
+    if res.entity not in allowed_settings:
+        raise HTTPException(status_code=404, detail="Setting not allowed")
 
     new_setting = Settings(
         account_id=auth["account"],
