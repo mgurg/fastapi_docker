e45b20bd4fcd25e12e85a5b7d7c63013a4ad1b7d
mgur <mgur@pop-os.localdomain>
2022-08-04 15:27:51 +0200

    Abs path

--- tests/conftest.py
+++ tests/conftest.py
@@ -63,7 +63,7 @@ from app.service.tenants import alembic_upgrade_head, tenant_create
 # DEFAULT_DATABASE_PASSWORD: str = os.getenv("DB_PASSWORD")
 # URL = f"postgresql+psycopg2://{DEFAULT_DATABASE_USER}:{DEFAULT_DATABASE_PASSWORD}@{DEFAULT_DATABASE_HOSTNAME}:5432/{DEFAULT_DATABASE_DB}"
 
-# load_dotenv(Path(__file__).parent.parent / "fastapi_docker" / "app" / ".env")
+load_dotenv(Path(__file__).parent.parent / "fastapi_docker" / "app" / ".env")
 logger.debug(Path(__file__).parent.parent)
 settings = Settings(
     DEFAULT_DATABASE_HOSTNAME=os.getenv("DB_HOST"),
