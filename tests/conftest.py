18a32d9dcb6e7a3baaca125b0ea6a7674ff232de
mgur <mgur@pop-os.localdomain>
2022-08-04 10:24:09 +0200

    test: Correct DSN during test

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,7 +22,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 from app.config import Settings
-from app.db import get_db
+from app.db import SQLALCHEMY_DATABASE_URL, get_db
 from app.main import app
 from app.service.bearer_auth import has_token
 from app.service.tenants import alembic_upgrade_head, tenant_create
@@ -39,20 +39,24 @@ from app.service.tenants import alembic_upgrade_head, tenant_create
 #     os.environ["AWS_SECURITY_TOKEN"] = "testing"
 #     os.environ["AWS_SESSION_TOKEN"] = "testing"
 
-load_dotenv("./app/.env")
-DEFAULT_DATABASE_USER = os.getenv("DB_USERNAME")
-DEFAULT_DATABASE_HOSTNAME: str = os.getenv("DB_HOST")
-DEFAULT_DATABASE_DB: str = os.getenv("DB_DATABASE")
-DEFAULT_DATABASE_PORT: str = os.getenv("DB_PORT")
-DEFAULT_DATABASE_PASSWORD: str = os.getenv("DB_PASSWORD")
 
+load_dotenv("./app/.env")
+os.environ["ENVIRONMENT"] = "PYTEST"
+
+DEFAULT_DATABASE_HOSTNAME: str = os.getenv("DB_HOST")
+DEFAULT_DATABASE_PORT: str = os.getenv("DB_PORT")
+DEFAULT_DATABASE_DB: str = os.getenv("DB_DATABASE")
+DEFAULT_DATABASE_USER: str = os.getenv("DB_USERNAME")
+DEFAULT_DATABASE_PASSWORD: str = os.getenv("DB_PASSWORD")
 URL = f"postgresql+psycopg2://{DEFAULT_DATABASE_USER}:{DEFAULT_DATABASE_PASSWORD}@{DEFAULT_DATABASE_HOSTNAME}:5432/{DEFAULT_DATABASE_DB}"
 
 
 @pytest.fixture(scope="module", autouse=True)
 def my_fixture():
+
     os.environ["TESTING"] = "1"
-    logger.info("INITIALIZATION")
+
+    logger.info("INITIALIZATION " + URL)
     alembic_upgrade_head("a", "head", URL)
     yield
     logger.critical("TEAR DOWN")
