e81c5850b0faf76477af09182727458aa0907788
mgur <mgur@pop-os.localdomain>
2022-11-03 19:47:22 +0100

    feat: QR codes

---
+++ migrations/versions/2022_11_03_1945-b6ec5354ff82_add_qr_codes.py
@@ -0,0 +1,37 @@
+"""Add QR codes
+
+Revision ID: b6ec5354ff82
+Revises: f0ae7b9d9467
+Create Date: 2022-11-03 19:45:16.720097
+
+"""
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.sql import column, table
+
+# revision identifiers, used by Alembic.
+revision = "b6ec5354ff82"
+down_revision = "f0ae7b9d9467"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "qr_codes",
+        sa.Column("id", sa.INTEGER(), sa.Identity(), autoincrement=True, nullable=False),
+        sa.Column("uuid", postgresql.UUID(as_uuid=True), autoincrement=False, nullable=True),
+        sa.Column("resource", sa.VARCHAR(length=512), autoincrement=False, nullable=True),
+        sa.Column("qr_code_id", sa.VARCHAR(length=512), autoincrement=False, nullable=True),
+        sa.Column("qr_code_content", sa.VARCHAR(length=512), autoincrement=False, nullable=True),
+        sa.Column("ecc", sa.CHAR(length=1), autoincrement=False, nullable=True),
+        sa.Column("created_at", postgresql.TIMESTAMP(timezone=True), autoincrement=False, nullable=True),
+        sa.Column("updated_at", postgresql.TIMESTAMP(timezone=True), autoincrement=False, nullable=True),
+        sa.PrimaryKeyConstraint("id", name="qr_codes_pkey"),
+        schema=None,
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("qr_codes", schema=None)
