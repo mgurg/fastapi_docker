255156ea1ba9ee8aaf2bc7090f5cadbb68cf1068
mgur <mgur@pop-os.localdomain>
2023-01-10 08:04:26 +0100

    SQLAlchemy 2.0 Commit fix

--- migrations/env.py
+++ migrations/env.py
@@ -84,6 +84,9 @@ def run_migrations_online() -> None:
         connection.execute(text('set search_path to "%s"' % current_tenant))
         connection.dialect.default_schema_name = current_tenant
 
+        # add this line
+        connection.commit()
+
         context.configure(
             connection=connection,
             target_metadata=target_metadata,
