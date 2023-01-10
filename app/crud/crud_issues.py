6cf580130d101e543d71cbccebc84f45fb56e520
mgur <mgur@pop-os.localdomain>
2023-01-10 12:26:16 +0100

    Unify endpoints

--- app/crud/crud_issues.py
+++ app/crud/crud_issues.py
@@ -44,31 +44,6 @@ def get_issues(
     return result.scalars().all()
 
 
-def get_issues_by_user_id(db, user_id: int, search: str, status: str, sort_column: str, sort_order: str) -> Issue:
-    query = (
-        select(Issue).order_by(text(f"{sort_column} {sort_order}")).filter(Issue.users_issue.any(User.id == user_id))
-    )
-
-    search_filters = []
-
-    if search is not None:
-        search_filters.append(Issue.name.ilike(f"%{search}%"))
-        search_filters.append(Issue.text.ilike(f"%{search}%"))
-
-        query = query.filter(or_(False, *search_filters))
-
-    match status:
-        case "active":
-            query = query.where(not_(Issue.status.in_(["resolved", "rejected"])))
-        case "inactive":
-            query = query.where(Issue.status.in_(["resolved", "rejected"]))
-        case "new" | "accepted" | "rejected" | "in_progress" | "paused" | "resolved" as issue_status:
-            query = query.where(Issue.status == issue_status)
-
-    result = db.execute(query)  # await db.execute(query)
-    return result.scalars().all()
-
-
 def get_issue_by_uuid(db: Session, uuid: UUID) -> Issue:
     query = select(Issue).where(Issue.uuid == uuid)
 
