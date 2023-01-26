769ea0f2219eb1dce065c3fbfb587569a1d998b4
mgur <mgur@pop-os.localdomain>
2023-01-26 21:25:22 +0100

    Mailjet

---
+++ app/api/statistics.py
@@ -0,0 +1,4 @@
+# Failures per machine
+
+# SELECT events.resource_uuid, count(events.id) from piekarnia__cukiernia_bonczyk_edaf2378f5124ca490f42984ceac235e.events where action ='issue_add'
+# group by events.resource_uuid;
--- app/service/notification_email.py
+++ app/service/notification_email.py
@@ -1,4 +1,5 @@
 import base64
+import json
 import os
 
 from requests import request
@@ -13,9 +14,12 @@ class EmailNotification:
         self.app_key = settings.email_labs_app_key
         self.secret_key = settings.email_labs_secret_key
         self.smtp = settings.email_smtp
-        self.auth_header = base64.b64encode(f"{self.app_key}:{self.secret_key}".encode())
-        self.url = "https://api.emaillabs.net.pl/api/sendmail_templates"
+        self.auth_header = self.generate_basic_auth(self.app_key, self.secret_key)
+
         self.sender = settings.email_sender
+
+    def generate_basic_auth(self, username: str, password: str):
+        return base64.b64encode(f"{username}:{password}".encode())
 
     def send(self, receiver: str, subject: str, template: str, vars: dict):
 
@@ -24,6 +28,14 @@ class EmailNotification:
 
         if settings.ENVIRONMENT != "PRD":
             receiver = settings.email_dev
+
+        # response = self.by_email_labs(receiver, subject, template, vars)
+        response = self.by_mailjet(receiver, subject, template, vars)
+
+        return response
+
+    def by_email_labs(self, receiver: str, subject: str, template: str, vars: dict):
+        url = "https://api.emaillabs.net.pl/api/sendmail_templates"
 
         receiver_data = {f"to[{receiver}]": ""}
 
@@ -41,22 +53,48 @@ class EmailNotification:
         payload = receiver_data | template_data
         files = {}
 
-        # data = {
-        # "to[email@domain.com]": "",
-        # "smtp_account": "1.account.smtp",
-        # "subject": "Test template 2",
-        # "html": "Some HTML",
-        # "text": "Some Text",
-        # "reply_to": "reply@domain.com",
-        # "from": "from_address@domain.com",
-        # "from_name": "Office",
-        #  'files[0][name]' : "name.png",
-        #  'files[0][mime]' : "image/png",
-        #  'files[0][content]' : base64.b64encode(file_get_contents("grafika.png")),
-        # }
+        response = request("POST", url, headers=headers, data=payload, files=files)
+        return response.text
 
-        # print(response.text)
-        # pprint(payload)
+    def by_mailjet(self, receiver: str, subject: str, template: str, vars: dict):
 
-        response = request("POST", self.url, headers=headers, data=payload, files=files)
+        url = "https://api.mailjet.com/v3.1/send"
+
+        payload_dict = {
+            "Messages": [
+                {
+                    "From": {"Email": "xxxxx+mailjet@gmail.com", "Name": "remontmaszyn.pl"},
+                    "To": [{"Email": "passenger1@example.com", "Name": "passenger 1"}],
+                    "TemplateID": 4534065,
+                    "TemplateLanguage": True,
+                    "Subject": "Nowa awaria",
+                    "Variables": {"issue_name": "a", "issue_url": "b"},
+                }
+            ]
+        }
+
+        payload = json.dumps(
+            {
+                "Messages": [
+                    {
+                        "From": {"Email": "xxxxx+mailjet@gmail.com", "Name": "remontmaszyn.pl"},
+                        "To": [{"Email": "passenger1@example.com", "Name": "passenger 1"}],
+                        "TemplateID": 4534065,
+                        "TemplateLanguage": True,
+                        "Subject": "Nowa awaria",
+                        "Variables": {"issue_name": "", "issue_url": ""},
+                    }
+                ]
+            }
+        )
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Basic XXXXX=",
+        }
+
+        response = request("POST", url, headers=headers, data=payload)
+
+        print(response.text)
+
         return response.text
