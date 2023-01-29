fb83b715a4e7af8173da670e71f5165623c53cd6
mgur <mgur@pop-os.localdomain>
2023-01-29 12:08:48 +0100

    Fix Issue summary stats

--- app/crud/crud_statistics.py
+++ app/crud/crud_statistics.py
@@ -4,6 +4,12 @@ from sqlalchemy.orm import Session
 from app.models.models import Issue, Item, User
 
 
+def get_issues_counter_summary(db: Session):
+    query = select(Issue.status, func.count(Issue.status)).group_by(Issue.status)
+    result = db.execute(query)  # await db.execute(query)
+    return result.all()
+
+
 def get_items_counter_summary(db: Session):
     query = select(Item.author_id, func.count(Item.author_id)).group_by(Item.author_id)
     result = db.execute(query)  # await db.execute(query)
@@ -14,15 +20,6 @@ def get_users_counter_summary(db: Session, user_id: int):
     query = select(func.count(User.id)).where(User.id != user_id)
     result = db.execute(query)  # await db.execute(query)
     return result.scalar_one_or_none()
-
-
-def get_issues_counter_summary(db: Session, status: list):
-    query = (
-        select(Issue.author_id, func.count(Issue.author_id)).where(Issue.status.in_(status)).group_by(Issue.author_id)
-    )
-
-    result = db.execute(query)  # await db.execute(query)
-    return result.all()
 
 
 def get_favourites_counter_summary(db: Session, user_id: int):
