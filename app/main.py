2ada11d806f11192043a2b2e44aa747f7827344c
mgur <mgur@pop-os.localdomain>
2023-07-06 09:33:19 +0200

    disable FastAPIPaginationWarning

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
+import warnings
 from typing import Any
 
 import sentry_sdk
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi_pagination.utils import FastAPIPaginationWarning
 from loguru import logger
 from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
@@ -26,6 +28,7 @@ from app.service.scheduler import scheduler, start_scheduler
 from app.service.tenants import alembic_upgrade_head
 
 settings = get_settings()
+warnings.simplefilter("ignore", FastAPIPaginationWarning)
 logger.add("./app/logs/logs.log", format="{time} - {level} - {message}", level="DEBUG", backtrace=False, diagnose=True)
 
 origins = ["http://localhost", "http://localhost:8080", "*"]
