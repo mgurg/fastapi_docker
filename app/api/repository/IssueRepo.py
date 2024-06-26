de58a99f1331f71283b02c25c34896e3c6c0bb64
mgurg <34517378+mgurg@users.noreply.github.com>
2024-06-26 11:34:43 +0200

    Refactor Issues - get all

--- app/api/repository/IssueRepo.py
+++ app/api/repository/IssueRepo.py
@@ -4,12 +4,14 @@ from typing import Annotated, Any
 from uuid import UUID
 
 from fastapi import Depends
-from sqlalchemy import Row, func, select
-from sqlalchemy.orm import Session, selectinload
+from sqlalchemy import Row
+from sqlalchemy import func, not_, or_, select, text
+from sqlalchemy.orm import Session
+from sqlalchemy.orm import selectinload
 
 from app.api.repository.generics import GenericRepo
 from app.db import get_db
-from app.models.models import Issue, Tag
+from app.models.models import Issue, Tag, User
 
 UserDB = Annotated[Session, Depends(get_db)]
 
@@ -49,3 +51,60 @@ class IssueRepo(GenericRepo[Issue]):
 
         result = self.session.execute(query)
         return result.scalar_one_or_none()
+
+    def get_issues(self,
+                   sort_column: str,
+                   sort_order: str,
+                   search: str | None,
+                   status: str | None,
+                   user_id: int | None,
+                   priority: str | None,
+                   date_from: datetime = None,
+                   date_to: datetime = None,
+                   tags: list[int] = None,
+                   ):
+        search_filters = []
+
+        query = select(Issue)
+
+        if search is not None:
+            search_filters.append(Issue.name.ilike(f"%{search}%"))
+            search_filters.append(Issue.text.ilike(f"%{search}%"))
+
+            query = query.filter(or_(False, *search_filters))
+
+        match status:
+            case "all":
+                ...
+            case "active":
+                query = query.where(not_(Issue.status.in_(["done", "rejected"])))
+            case "inactive":
+                query = query.where(Issue.status.in_(["done", "rejected"]))
+            case "new" | "accepted" | "rejected" | "in_progress" | "paused" | "done" as issue_status:
+                query = query.where(Issue.status == issue_status)
+
+        match priority:
+            case "low":
+                query = query.where(self.Model.priority == "10")
+            case "medium":
+                query = query.where(self.Model.priority == "20")
+            case "high":
+                query = query.where(self.Model.priority == "30")
+            case _:
+                ...
+
+        if user_id is not None:
+            query = query.filter(Issue.users_issue.any(User.id == user_id))
+
+        if date_from is not None:
+            query = query.filter(func.DATE(Issue.created_at) >= date_from)
+
+        if date_to is not None:
+            query = query.filter(func.DATE(Issue.created_at) <= date_to)
+
+        if tags is not None:
+            query = query.where(Issue.tags_issue.any(Tag.id.in_(tags)))
+
+        query = query.order_by(text(f"{sort_column} {sort_order}"))
+
+        return query
--- app/api/service/issue_service.py
+++ app/api/service/issue_service.py
@@ -2,32 +2,44 @@ from datetime import datetime
 from typing import Annotated
 from uuid import UUID
 
-from fastapi import Depends
+from fastapi import Depends, HTTPException
+from starlette.status import HTTP_404_NOT_FOUND
 
 from app.api.repository.IssueRepo import IssueRepo
 from app.api.repository.TagRepo import TagRepo
+from app.api.repository.UserRepo import UserRepo
 
 
 class IssueService:
     def __init__(
-        self,
-        issue_repo: Annotated[IssueRepo, Depends()],
-        tag_repo: Annotated[TagRepo, Depends()],
+            self,
+            user_repo: Annotated[UserRepo, Depends()],
+            issue_repo: Annotated[IssueRepo, Depends()],
+            tag_repo: Annotated[TagRepo, Depends()],
     ) -> None:
-        self.issue_repo = (issue_repo,)
+        self.user_repo = user_repo
+        self.issue_repo = issue_repo
         self.tag_repo = tag_repo
 
     def get_all(
-        self,
-        sort_column: str,
-        sort_order: str,
-        search: str | None,
-        status: str | None,
-        user: int | None,
-        priority: str | None,
-        date_from: datetime = None,
-        date_to: datetime = None,
-        tags: list[UUID] = None,
+            self,
+            sort_column: str,
+            sort_order: str,
+            search: str | None,
+            status: str | None,
+            user_uuid: UUID | None,
+            priority: str | None,
+            date_from: datetime = None,
+            date_to: datetime = None,
+            tags: list[UUID] = None,
     ):
-        if tags is not None:
-            tag_ids = self.tag_repo.get_by_uuid(tag)  # FIX
+        tag_ids = self.tag_repo.get_ids_by_tags_uuid(tags) if tags else None
+
+        user_id = None
+        if user_uuid is not None:
+            db_user = self.user_repo.get_by_uuid(user_uuid)
+            if db_user is None:
+                raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=f"User `{user_uuid}` not found")
+            user_id = db_user.id
+
+
